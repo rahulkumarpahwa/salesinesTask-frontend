918dbfc09129f0491255531e955559cbb7f4bd7d
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-06-25 16:31:04 +0530

    fixing UI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,13 +8,22 @@ import { RouterLink } from 'vue-router'
   <main>
     <div class="flex justify-center items-center h-[20rem] flex-col gap-2">
       <!-- <div class="my-3 text-[#41b883] text-2xl"><GiRapidshareArrow />↪ Hover Here </div> -->
+       <div class="flex gap-10 items-center justify-center">
       <img
         alt="Vue logo"
-        class="w-[5rem] animate-spin hover:duration-300 hover:scale-150"
+        class="w-[5rem] animate-spin hover:duration-700 hover:scale-150"
         src="@/assets/logo.svg"
         width="125"
         height="125"
       />
+      <img
+        alt="Vue logo"
+        class="w-[5rem] animate-spin hover:duration-700 hover:scale-150"
+        src="@/assets/next.svg"
+        width="125"
+        height="125"
+      />
+      </div>
       <h1 class="mt-10 text-3xl font-bold">Welcome to Our Task Manager App.</h1>
       <p class="">
         To start creating your Tasks, You can either
